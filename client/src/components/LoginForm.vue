16a38e9a97ff59e2e348053fe7d707c8fe058c9b
dongjin <526631388@qq.com>
2020-09-25 17:37:05 +0800

    fix login

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <Modal
+                :closable="false"
+                :mask-closable="false"
+                v-model="modal"
+                @on-cancel="cancel">
+            <div class="login-content">
+                <img src="../assets/logo.png" alt="">
+                <div class="item-wrapper">
+                    <input type="text" placeholder="Username">
+                    <input type="password" placeholder="Password">
+                </div>
+            </div>
+            <div slot="footer">
+                <Button type="primary" size="large" @click="handleSubmit('formValidate')">Sign in</Button>
+                <Button type="text" size="large" @click="cancel">取消</Button>
+            </div>
+        </Modal>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LoginForm",
+        props: ["modal", "modify"],
+        watch: {
+            modify: function () {
+                console.log("modify", this.modify)
+            },
+        },
+        data() {
+            return {
+                formItem: {
+                    username: '',
+                    password: '',
+                },
+            }
+        },
+        methods: {
+            cancel() {
+                this.$parent.cancel();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    .login-content {
+        margin-left: 20px;
+    }
+
+    img {
+        margin-top: 20px;
+        width: 140px;
+        height: 42px;
+    }
+
+    .item-wrapper {
+        width: 100%;
+        justify-content: left;
+        margin-top: 40px;
+    }
+
+    input {
+        width: 500px;
+        height: 60px;
+        background: #F6F6F6;
+        border: 1px solid #F6F6F6;
+        border-radius: 8px;
+        margin-bottom: 30px;
+        font-size: 30px;
+        padding: 0 20px;
+    }
+</style>
---
+++ client/src/components/VideoPlay.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="video-play">
+        <div class="player-container">
+            <video-player class="vjs-custom-skin" :options="playerOptions"></video-player>
+        </div>
+    </div>
+</template>
+
+<script>
+    //引入video样式
+    import 'video.js/dist/video-js.css'
+    import 'vue-video-player/src/custom-theme.css'
+
+    export default {
+        name: "VideoPlay",
+        data() {
+            return {
+                playerOptions: {
+                    playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+                    autoplay: false, //如果true,浏览器准备好时开始回放。
+                    controls: true, //控制条
+                    preload: 'auto', //视频预加载
+                    muted: false, //默认情况下将会消除任何音频。
+                    loop: false, //导致视频一结束就重新开始。
+                    language: 'zh-CN',
+                    aspectRatio: '16:7', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources: [{
+                        type: 'video/mp4',
+                        src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+                    }],
+                    poster: "http://39.106.117.192:8080/static/indexImg.png", //你的封面地址（覆盖在视频上面的图片）
+                    width: document.documentElement.clientWidth,
+                    notSupportedMessage: '此视频暂无法播放，请稍后再试' //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .video-play {
+        margin-top: 20px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .player-container {
+        width: 80%;
+    }
+</style>
