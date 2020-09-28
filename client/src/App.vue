b1244e3efbb2892512ed20aae7f1aff62cd57a6d
dongjin <526631388@qq.com>
2020-09-28 16:14:24 +0800

    fix play

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,4 +19,8 @@
         -moz-osx-font-smoothing: grayscale;
         color: #2c3e50;
     }
+    .vjs-modal-dialog .vjs-modal-dialog-content, .video-js .vjs-modal-dialog, .vjs-button > .vjs-icon-placeholder:before, .video-js .vjs-big-play-button .vjs-icon-placeholder:before {
+        position: relative;
+    }
+
 </style>
